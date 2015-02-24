41dbee96c104d4e4fc50344bcdc14b4474a640b7
danwebdev <dandeveloping@gmx.de>
2015-02-24 01:08:32 +0100

    added fluid Layout + Animation

--- panorama/js/main.js
+++ panorama/js/main.js
@@ -1,31 +1,35 @@
 (function($, window, document) {
 
-	var options = {
-        displayRays : true,
-        displayAltitudeHierarchy : false,
-        connectTheDots : false,
-
-		//Center Circle
-		centerCircleStrokeColor: 'red',
-		centerCircleStrokeWidth: 0.3,
-		centerCircleRadius: 3,
-		//Rays
-		rayStrokeColor: 'grey',
-        rayStrokeColorTaller : '#ff9999',
-        rayStrokeColorSmaller : '#9999ff',
-		rayStrokeWidth: 0.1,
-		//Peaks
-		peakStrokeColor: 'grey',
-		peakStrokeWidth: 0.3,
-		peakRadius: 3,
+    var options = {
+        //General
+        displayRays: true,
+        displayPeaks: true,
+        displayAltitudeHierarchy: false,
+        connectTheDots: false,
+        rigiAltitude: 1798,
+        outerPadding: 20,
+        //Center Circle
+        centerCircleStrokeColor: 'grey',
+        centerCircleStrokeWidth: 0.3,
+        centerCircleRadius: 50,
+        //Rays
+        rayStrokeColor: 'grey',
+        rayStrokeColorTaller: '#ff9999',
+        rayStrokeColorSmaller: '#9999ff',
+        rayStrokeWidth: 0.1,
+        //Peaks
+        peakStrokeColor: 'grey',
+        peakStrokeWidth: 0.3,
+        peakRadius: 3,
         //ConnectTheDots Path
-        connectTheDotsColor : '#ffcccc',
+        connectTheDotsColor: '#ffcccc',
         connectTheDotsWidth: 0.3,
-        connectTheDotsSmoothPath: true,
+        connectTheDotsSmoothPath: true
+    };
 
-        rigiAltitude : 1798
+    var data;
 
-	};
+    var sizes = {};
 
     var maxDistance = function(dataArray) {
         var heightsArray = dataArray.map(function(el) {
@@ -33,92 +37,138 @@
         });
         return Math.max.apply(Math, heightsArray);
     }
-    var minDistance = function(dataArray) {
-        var heightsArray = dataArray.map(function(el) {
-            return el.distance_km;
-        });
-        return Math.min.apply(Math, heightsArray);
+
+    var resizeHandler = function(evt) {
+        console.log('resized, calling draw ...');
+        sizes.maxExtent = (paper.view.viewSize._width >= paper.view.viewSize._height) ? paper.view.viewSize._height / 2 : paper.view.viewSize._width / 2;
+        sizes.maxRayLength = sizes.maxExtent - options.centerCircleRadius - options.outerPadding;
+
+        draw(data);
     }
 
-    var destinationPoints;
+    var initDraw = function() {
+        sizes.maxDistance = maxDistance(data);
+        // Create an empty project and a view for the canvas:
+        var canvas = document.getElementById('myCanvas');
+        paper.setup(canvas);
+        paper.view.onResize = resizeHandler;
+        
+        resizeHandler();
+
+
+    }
 
     var init = function() {
         $.getJSON("data/gipfel.json", function(_data) {
-            draw(_data);
+            data = _data;
+            initDraw();
         });
-        
     }
-
-
 
     $(function() {
         init();
     });
 
 
+
+
+
+    /*=========================================
+    =            Main Draw Routine            =
+    =========================================*/
     function draw(data) {
-    	
+            paper.view.onFrame = function(){
+            peaksnraysGroup.rotate(0.05 , paper.view.center);
+        }
+        console.log('draw');
 
-        var canvas = document.getElementById('myCanvas');
-        // Create an empty project and a view for the canvas:
-        paper.setup(canvas);
-
+        paper.project.clear();
         // Create a Paper.js Path to draw a line into it:
-        
-        var centerPoint = paper.view.center;
-        var paddingVector = new paper.Point;
-        paddingVector.length = options.centerCircleRadius;
 
-        if(options.connectTheDots){
+        var centerPoint = paper.view.center;
+
+        var innerPaddingVector = new paper.Point;
+        innerPaddingVector.length = options.centerCircleRadius;
+
+        //Connect the dots
+        if (options.connectTheDots) {
             var connectTheDotsPath = new paper.Path;
             connectTheDotsPath.strokeColor = options.connectTheDotsColor;
-            connectTheDotsPath.fillColor =  options.connectTheDotsColor;
+            connectTheDotsPath.fillColor = options.connectTheDotsColor;
             connectTheDotsPath.strokeWidth = options.connectTheDotsWidth;
         }
-       
 
+        //Iterate data and draw peaks and rays
+        var rays = [];
+        var peaks = [];
+        var peaksnrays = [];
+        var peaksnraysGroup = new paper.Group;
+
+        var destVector = new paper.Point;
         for (var i = data.length - 1; i >= 0; i--) {
-        	var destVector = new paper.Point;
-        	destVector.angle = data[i].degrees;
-        	paddingVector.angle = data[i].degrees;
-        	destVector.length = data[i].distance_km*4;
 
-            console.log('destVercotr end '+destVector.endPoint);
-        	var destPoint = centerPoint.add(paddingVector).add(destVector);
-            if(options.displayRays){
-            	var ray = new paper.Path();
-            	ray.strokeColor = ! options.displayAltitudeHierarchy ? options.rayStrokeColor : data[i].altitude > options.rigiAltitude ? options.rayStrokeColorTaller : options.rayStrokeColorSmaller;
-            	ray.strokeWidth = options.rayStrokeWidth;
-            	ray.moveTo(centerPoint.add(paddingVector));
-                
-            	ray.lineTo(destPoint);
+
+            destVector.angle = data[i].degrees;
+            innerPaddingVector.angle = data[i].degrees;
+            destVector.length = data[i].distance_km / sizes.maxDistance * sizes.maxRayLength;
+            var destPoint = centerPoint.add(innerPaddingVector).add(destVector);
+
+            if (options.displayRays) {
+                var ray = new paper.Path();
+                ray.strokeColor = !options.displayAltitudeHierarchy ? options.rayStrokeColor : data[i].altitude > options.rigiAltitude ? options.rayStrokeColorTaller : options.rayStrokeColorSmaller;
+                ray.strokeWidth = options.rayStrokeWidth;
+                ray.moveTo(centerPoint.add(innerPaddingVector));
+                ray.lineTo(destPoint);
+                rays.push(ray);
+                peaksnraysGroup.addChildren(rays);
             }
 
-            if(options.connectTheDots){
+            if (options.connectTheDots) {
                 connectTheDotsPath.add(new paper.Point(destPoint));
             }
 
-        	var peakCircle = new paper.Path.Circle(destPoint, options.peakRadius);
-        	peakCircle.strokeColor = options.peakStrokeColor;
-        	peakCircle.strokeWidth = options.peakStrokeWidth;
+            if (options.displayPeaks) {
+                var peakCircle = new paper.Path.Circle(destPoint, options.peakRadius);
+                peakCircle.strokeColor = options.peakStrokeColor;
+                peakCircle.strokeWidth = options.peakStrokeWidth;
+                peaks.push(peakCircle);
+                peaksnraysGroup.addChildren(peaks);
+            }
         };
 
-        if(options.connectTheDots){
-                connectTheDotsPath.closed = true;
-                connectTheDotsPath.fullySelected = false;
-                if(options.connectTheDotsSmoothPath){
-                    connectTheDotsPath.smooth();
-                }
+
+        if (options.connectTheDots) {
+            connectTheDotsPath.closed = true;
+            connectTheDotsPath.fullySelected = false;
+            if (options.connectTheDotsSmoothPath) {
+                connectTheDotsPath.smooth();
+            }
 
         }
 
+        //Draw center circle aka Rigi
         var centerCircle = new paper.Path.Circle(centerPoint, options.centerCircleRadius);
-    	centerCircle.strokeColor = options.centerCircleStrokeColor;
-    	centerCircle.strokeWidth = options.centerCircleStrokeWidth;
+        centerCircle.strokeColor = options.centerCircleStrokeColor;
+        centerCircle.strokeWidth = options.centerCircleStrokeWidth;
+
+        //Draw upward beam
+        var beamDest = centerPoint.add(new paper.Point(sizes.maxExtent - options.outerPadding, 0));
+        var beam = new paper.Path();
+        beam.strokeColor = 'red';
+        beam.strokeWidth = 1;
+        beam.moveTo(centerPoint.add(innerPaddingVector));
+        beam.lineTo(beamDest);
+        //
 
 
-		paper.view.draw();
+        //Rotate main layer to make o°=NORTH
+        paper.project.activeLayer.rotate(-90, paper.view.center);
+
+        //Invoke draw
+
+        paper.view.draw();
 
     }
+
 
 }(window.jQuery, window, document));
